ea72c33ae35713bf0ee3c1ca561d9afa60332683
DevStuffzz <cb050129@gmail.com>
2025-12-07 22:51:17 -0500

    Add platform.c, null backend, and win32_module.c to premake build

--- premake5.lua
+++ premake5.lua
@@ -14,6 +14,7 @@ project "GLFW"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/platform.c",
         "src/vulkan.c",
         "src/window.c"
     }
@@ -30,9 +31,14 @@ project "GLFW"
             "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
+            "src/win32_module.c",
             "src/wgl_context.c",
             "src/egl_context.c",
-            "src/osmesa_context.c"
+            "src/osmesa_context.c",
+            "src/null_init.c",
+            "src/null_monitor.c",
+            "src/null_window.c",
+            "src/null_joystick.c"
         }
         
         defines 
